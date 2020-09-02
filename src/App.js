95d9a31930f8e1dd2d2fd9ce3718136465040a26
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-09-01 21:54:37 -0500

    Started the framework for the app.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+const api = {
+  key: "2373df1c90efe22e28431e86c2bd7107",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input 
+          type="text"
+          className="search-bar"
+          placeholder="Search..."
+          />
+        </div>
+      </main>
     </div>
   );
 }
